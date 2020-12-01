4dce67d6321989ae1819794def62b3ee392ce77b
vkm9594 <vkm9594@github.com>
2020-11-30 22:49:37 -0500

    Gradient background

--- sketch.js
+++ sketch.js
@@ -3,5 +3,23 @@ function setup() {
 }
 
 function draw() {
-  background(200);
+  background(0);
+
+  startColor = color(51, 58, 135);
+  stopColor = color(122, 51, 135);
+  verticalGradientRect(218, 53, 1000, 550, startColor, stopColor);
+  
+}
+
+function verticalGradientRect(x, y, w, h, startColor, stopColor) {
+  push();
+  strokeWeight(1);
+  for(let i = 0; i < h; i++) {
+    stroke(lerpColor(startColor, stopColor, i / (h - 1)));
+    line(x, y + i, w + x, y + i);
+  }
+  noFill();
+  noStroke();
+  rect(x, y, w, h);
+  pop();
 }
