330f3eb879521b7418c5b841a57bd6322509c3b6
vkm9594 <vkm9594@github.com>
2020-12-08 13:41:10 -0500

    Add title screen

--- sketch.js
+++ sketch.js
@@ -8,10 +8,12 @@ let sprite;
 
 function preload() {
   sprite = loadImage("kore.png");
+  screen = 0;
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  screen = 0;
+  createCanvas(windowWidth - 2, windowHeight - 3);
 
   for (let i = 0; i < 500; i++) {
     stars[i] = new Star();
@@ -27,33 +29,46 @@ function setup() {
 
 function draw() {
   background(0);
-
-  // startColor = color(51, 58, 135);
-  // stopColor = color(122, 51, 135);
-  startColor = color(0, 145, 212);
-  stopColor = color(247, 245, 163);
-  verticalGradientRect(0, 0, windowWidth, windowHeight, startColor, stopColor);
-
-  moon();
-
-  // calls class Star
-  for (let i = 0; i < stars.length; i++) {
-    stars[i].show();
+  if (screen == 0) {
+    textSize(72);
+    fill(255);
+    text("KORE", width / 2 - 100, height / 2);
+    textSize(24);
+    text("Press a key to begin", width / 2 -110, height / 2 + 50);
   }
 
-  // draws the waves
-  wave1();
-  wave2();
-  wave3();
+  if (screen == 1) {
+    // startColor = color(51, 58, 135);
+    // stopColor = color(122, 51, 135);
+    startColor = color(0, 145, 212);
+    stopColor = color(247, 245, 163);
+    verticalGradientRect(0, 0, windowWidth, windowHeight, startColor, stopColor);
 
-  grassPlatform();
+    moon();
 
-  for (let i = 0; i < tree.length; i++) {
-    tree[i].show();
+    // calls class Star
+    for (let i = 0; i < stars.length; i++) {
+      stars[i].show();
+    }
+
+    // draws the waves
+    wave1();
+    wave2();
+    wave3();
+
+    grassPlatform();
+
+    for (let i = 0; i < tree.length; i++) {
+      tree[i].show();
+    }
+    //tree[0].mousePressed();
+
+    kore.show();
   }
-  //tree[0].mousePressed();
+}
 
-  kore.show();
+function keyPressed() {
+  screen = 1;
 }
 
 function mousePressed() { //adds new branches every time the mouse is pressed
